7115e1fa523025416b6da220225dbd54a2208da2
kangho lee <ibcylon@naver.com>
2021-11-18 21:07:36 +0900

    Initial Commit

---
+++ Hourglass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hourglass
+//
+//  Created by Kanghos on 2021/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
